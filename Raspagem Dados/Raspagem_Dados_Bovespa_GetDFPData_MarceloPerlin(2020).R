e5fe98673966ee3e704a86c56a5795823e08f2eb
Cao Bittencourt Ferreira <88053259+CaoBittencourtFerreira@users.noreply.github.com>
2021-09-22 13:48:25 -0300

    sa

--- Raspagem Dados/Raspagem_Dados_Bovespa_GetDFPData_MarceloPerlin(2020).R
+++ Raspagem Dados/Raspagem_Dados_Bovespa_GetDFPData_MarceloPerlin(2020).R
@@ -1,18 +1,25 @@
 # # Objetivo: valuation e visualizações diversas por empresa e setor
-# Pseudo-código: for each sector => for each company apply valuation function(s)
+# Pseudo-código: gather data => clean data => for each sector => for each company apply valuation function(s) => visualize 
+# Obs:o pacote 'GetDFPData2' organiza os demonstrativos de todas as empresas em um mesmo dataframe (uma lista de df_s, correspondendo a cada um dos demonstrativos financeiros)
+# => lapply(lista_dfs, val_function(group_by(setor,empresa)))
 
-# 1. PACOTES
-pkg <- c('quantmod', 'BatchGetSymbols', 
+
+# PACOTES -----------------------------------------------------------------
+pkg <- c('quantmod', 'BatchGetSymbols',  
          'PerformanceAnalytics',
-         'tidyquant', 'GetDFPData2',
+         'tidyquant', 'GetDFPData2', #'GetBCBData', #'GetHFData ', 
          'ggthemes', 'scales', 'glue', 'tidyverse')
 
 lapply(pkg, function(x)
   if(!require(x, character.only = T))
   {install.packages(x); require(x)})
 
-# 2. DADOS
-# # Escopo
+# lapply(pkg, function(x)
+#   {citation(package = x)})
+
+
+# DADOS -------------------------------------------------------------------
+# 1. Escopo
 # Empresas ainda ativas
 get_info_companies(cache_folder = tempdir()) %>%
   filter(SIT_REG == 'ATIVO') -> bovespa.empresas.ativas.info
@@ -20,6 +27,8 @@ get_info_companies(cache_folder = tempdir()) %>%
 # Dados disponíveis (de maneira conveniente) a partir de 2010 (vide http://dados.cvm.gov.br/dados/CIA_ABERTA/)
 # => Escopo máximo = 10 anos
 # Empresas com mais de 10 anos de atividade
+# Obs: existem métodos de avaliação apropriados para empresas novas (ver Damodaran)
+# => Talvez modificar o escopo depois
 anos.escopo <- 10 
 ano.passado <- year(today()) - 1
 ano.inicial <- ano.passado - anos.escopo
@@ -78,11 +87,11 @@ bovespa.empresas.ativas.info %>%
 #              use_memoise = T,
 #              cache_folder = tempdir())
 
-# 1 Setor aleatório
+# 1x Setor aleatório
 setores.teste %>%
   sample(1) -> setor.teste
 
-# 4 Companhias aleatórias
+# 4x Companhias aleatórias
 bovespa.empresas.ativas.info %>% 
   filter(SETOR_ATIV == setor.teste) %>%
   pull(CD_CVM) %>% sample(4) %>% 
@@ -93,10 +102,11 @@ bovespa.empresas.ativas.info %>%
                use_memoise = T,
                cache_folder = tempdir()) -> teste
 
+# LIMPEZA DOS DADOS -------------------------------------------------------
 # teste.backup <- teste
 # teste.backup -> teste
 
-# # Limpeza e modificação dos dados
+# 2. Limpeza e modificação dos dados
 # Obs: algumas empresas mudam de nome durante o período (cnpj e código da cvm permanecem os mesmos) => Homogeneizar os nomes segundo a nomenclatura mais recente 
 lapply(teste, function(df){
   
@@ -145,36 +155,39 @@ lapply(teste, function(df, nivel_especificidade = 1){
   
 }) -> teste
 
-# 3. ALGORITMOS DE VALUATION
-# # # FLUXO DE CAIXA DESCONTADO (FDC/DCF)
+# Obs: trim_ws nos nomes das contas
+# Obs: por vezes, verifica-se irregularidades nos nomes das contas (além whitespace)
+# Solução: usar código das contas (ver se existem irregularidades também) e/ou usar contains("parte do nome da conta") ao manipular os dados
 
-# # # MÚLTIPLOS DE MERCADO
+# depois disso, GetBatchSymbols/quantmod/etc para pegar as cotações dentro do escopo => df_cotacoes
 
-# # Preço por lucro
+# ALGORITMOS DE VALUATION -------------------------------------------------
+# 1. FLUXO DE CAIXA DESCONTADO (FDC/DCF)
+# Ver planilhas Damodaran
+# Obs: possivelmente será necessário utilizar a taxa selic ou outras (mas já existem diversos APIs para isso também; e.g. o mesmo autor do pacote GetDFPData2 tem outros para esses dados)
+
+# 2. MÚLTIPLOS DE MERCADO 
+# 2.1 Preço por lucro
 # P/L = Preço / lucro por ação
 
-# # Múltiplo do Valor patrimonial
+# 2.2. Múltiplo do Valor patrimonial
 # P/VPA = Preço de mercado/patrimônio líquido por ação
 # Valores importantes:
 # P/VPA > 1 => mercado valoriza a empresa além da equivalência patrimonial (i.e. acrescenta um bônus ao preço)
 # P/VPA < 1 => mercado valoriza a empresa abaixo da equivalência patrimonial (i.e. acrescenta uma penalidade ao preço)
 # P/VPA = 1 => mercado valoriza a empresa exatamente na equivalência patrimonial (i.e. sem bönus nem penalidade ao preço)
 
-# # DIVIDEND YIELD
+# 2.3. DIVIDEND YIELD
 # Dividend yield = Dividendo pago por ação / Preço por unidade de ação
 
-# # ENTERPRISE VALUE POR EBITDA
+# 2.4. ENTERPRISE VALUE POR EBITDA
 # EV/EBITDA = (Enteprise Value/Earnings Before Taxes Interest Depreciation and Amortization).
 # EV = valor de mercado da empresa = preço da ação * número de ações (i.e. o quanto o consenso do mercado diz que a empresa vale) + DÍVIDAS (BP) - CAIXA (BP)
 # EBITDA = lucratividade da empresa (desconsiderando variáveis que não têm a ver com a sua operação)
 # Quanto menor o EV/EBITDA, melhor (comparar setorialmente apenas)
 
-
-# # # AVALIAÇÃO CONTÁBIL
-# # # AVALIAÇÃO DE LIQUIDAÇÃO
-
-# # # INDICADORES IMPORTANTES
-# # Retorno da empresa
+# 3. INDICADORES IMPORTANTES
+# 3.1. Retorno da empresa
 # ROE (Return on Equity) = capacidade da empresa de agregar valor (gerar retorno) para o capital próprio investido
 # ROE = LUCRO LÍQUIDO (DRE) / PATRIMONIO LIQUIDO (BP) = LL/PL
 # Valores importantes:
@@ -182,36 +195,63 @@ lapply(teste, function(df, nivel_especificidade = 1){
 # ROE > 15% = "empresa muito boa" 
 # E assim por diante (comparar setorialmente)
 
-# ROIC (Return on Invested Capital) = medida de desempenho financeiro da empresa (considerando todo o capital investido, incluindo o de terceiros)
+# 3.2. ROIC (Return on Invested Capital) = medida de desempenho financeiro da empresa (considerando todo o capital investido, incluindo o de terceiros)
 # ROIC = Resultado Operacional / Valor Contábil do Capital Investido.
 
 
-# # Endividamento/Liquidez da empresa
-# Dívida Líquida
+# 4. Endividamento/Liquidez da empresa
+# 4.1. Dívida Líquida
 # DL = DIVIDA TOTAL - CAIXA (BP)
 
-# Dívida Líquida sobre Patrimônio Liquido
+# 4.2. Dívida Líquida sobre Patrimônio Liquido
 # DL/PL = alavangem/endividamento
 # Valores importantes:
 # DL/PL = 50% = alavancagem de 0.5 (metade da empresa não pertence à empresa)
 # DL/PL = 100% = empresa 100% alavancada (tudo na empresa é dívida)
 # Geralmente, quando menor melhor (e.g. DL/PL < 50%)
 
-# # MARGENS
-# Margem Bruta = rentabilidade após os custos (quanto de cada R$1,00 de receita sobreviveu às despesas da firma e virou lucro efetivamente)
+# 4.3. Liquidez corrente
+# LC = Ativo circulante (BP) / Passivo circulante (BP)
+# LC >= 1 => empresa capaz de liquidar as suas operações de curto prazo
+# LC < 1 => empresa incapaz de liquidar as suas operações de curto prazo
+
+# 4.4. Liquidez seca
+# LC = (Ativo circulante (BP) - Estoques (BP)) / Passivo circulante (BP)
+# LC >= 1 => empresa capaz de liquidar as suas operações de curto prazo
+# LC < 1 => empresa incapaz de liquidar as suas operações de curto prazo
+
+# 5. MARGENS
+# 5.1. Margem Bruta = rentabilidade após os custos (quanto de cada R$1,00 de receita sobreviveu às despesas da firma e virou lucro efetivamente)
 # MB = Lucro Bruto (DRE) / Receita Líquida (DRE) = LB/RL
 
-# Margem Operacional = lucro operacional obtido para cada unidade de venda realizada (i.e. quão rentável é a *operação* da firma)
+# 5.2. Margem EBITDA = rentabilidade após os custos e desconsiderando juros, impostos e depreciações (quanto de cada R$1,00 de receita sobreviveu às despesas da firma e virou EBITDA efetivamente)
+# ME = EBITDA (DRE) / Receita Líquida (DRE) = EBITDA/RL
+
+# 5.3. Margem Operacional = lucro operacional obtido para cada unidade de venda realizada (i.e. quão rentável é a *operação* da firma)
 # MO = Resultado operacional (DRE) / Receita Líquida (DRE)
 # Geralmente, margem maior, firma mais lucrativa
 # Exceção: quando a empresa diminui a margem para ganhar espaço no mercado (e.g. expansão, aquisição, marketing etc)
 
-# Margem Líquida = idem margem bruta, mas considerando apenas quando % de cada R$1,00 converteu-se em lucro *líquido* (tendo em vista todas as demais despesas da firma, e.g. impostos, depreciações)
+# 5.4. Margem Líquida = idem margem bruta, mas considerando apenas quando % de cada R$1,00 converteu-se em lucro *líquido* (tendo em vista todas as demais despesas da firma, e.g. impostos, depreciações)
 # ML = Lucro Liquido (DRE) / Receita Líquida (DRE) = LL/RL
 # Geralmente, quanto maior melhor (regra de bolso: > 10%)
 
-# 4. VALUATION SETORIAL
-# 5. VISUALIZAÇÕES DIVERSAS
+
+# AVALIAÇÃO POR SETOR -----------------------------------------------------
+# 1. SETORES
+# adicionar coluna de setor do df_info (viz. SETOR_ATIV) para os dfs_demonstrativos (e.g. pela coluna do cnpj ou cvm)
+
+# 2. APLIAÇÃO DOS MÉTODOS DE AVALIAÇÃO
+# group_by SETOR_ATIV => aplicar métodos funções de valuation acima
+
+
+# SIMULAÇÃO DE UMA CARTEIRA DE INVESTIMENTOS ------------------------------
+# 1. Selecionar as n "melhores ações" por setor e montar uma carteira
+# 2. Utilizar os pacotes "PerformanceAnalytics", "quantmod", "BatchGetSymbols" e afins para simular a performance da carteira em um período
+# Obs: provavelmente fazer uma função para sugerir empresas e montar a carteira (e.g. baseado em parâmetros como número de empresas, risco, número de setores, empresas por setor, métodos de valuation preferidos etc)
+
+
+# VISUALIZAÇÕES DIVERSAS --------------------------------------------------
 # Exemplo
 teste$`DF Consolidado - Demonstração do Resultado` %>%
   rename(Ano = DT_FIM_EXERC,
@@ -227,6 +267,19 @@ labs(title = glue('EBITDA (R$ Bi, {ano.inicial}-{ano.passado})'),
      caption = 'Fonte: CVM (dados importados com o pacote GetDFPData2).') +
 ggthemes::theme_economist()
 
+
+# BIBLIOGRAFIA (alguns exemplos) ------------------------------------------
+# 1. Valuation (Damodaran)
+# https://www.amazon.com.br/Valuation-Avaliar-Empresas-Escolher-Melhores/dp/8521620497
+
+# 2. Planilhas (Damodaran)
+# https://pages.stern.nyu.edu/~adamodar/
+
+# 3. Material da disciplina eletiva de Avaliação de Empresas
+# https://ss.cursos.fgv.br/d2l/le/content/84568/Home?itemIdentifier=D2L.LE.Content.ContentObject.ModuleCO-778475
+
+# 4. Tidy Valuation (DCF)
+# https://www.business-science.io/finance/2020/02/21/tidy-discounted-cash-flow.html
 
 
 
